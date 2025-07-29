a25b097800bc797d10a35f2a3a256b3c029d4fcc
Synera <isynera@gmail.com>
2025-07-29 20:14:14 +0000

    Change the color of the Sign in with Google button

--- src/components/auth-page.tsx
+++ src/components/auth-page.tsx
@@ -10,6 +10,12 @@ import { Button } from './ui/button';
 import { useAuth } from '@/contexts/auth-context';
 import { Separator } from './ui/separator';
 
+const GoogleIcon = (props: React.SVGProps<SVGSVGElement>) => (
+    <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" {...props}>
+        <path d="M12.48 10.92v3.28h7.84c-.24 1.84-.85 3.18-1.73 4.1-1.02 1.02-2.62 2.04-4.54 2.04-3.64 0-6.58-3-6.58-6.6s2.94-6.6 6.58-6.6c2.02 0 3.42.82 4.22 1.58l2.6-2.58C18.04 1.32 15.48 0 12.48 0 5.88 0 .04 5.84.04 12s5.84 12 12.44 12c3.28 0 5.74-1.14 7.6-3.04 1.94-1.9 2.6-4.56 2.6-7.38 0-.82-.1-1.46-.24-2.1H12.48z" />
+    </svg>
+);
+
 export function AuthPage() {
   const { loginWithGoogle, loading } = useAuth();
 
@@ -34,7 +40,8 @@ export function AuthPage() {
                 <CardDescription>Sign in with Google or use your email to continue.</CardDescription>
             </CardHeader>
             <CardContent>
-                 <Button variant="outline" className="w-full" onClick={loginWithGoogle} disabled={loading}>
+                 <Button variant="default" className="w-full" onClick={loginWithGoogle} disabled={loading}>
+                    <GoogleIcon className="mr-2 h-4 w-4 fill-current" />
                     Sign In with Google
                 </Button>
                 <div className="relative my-4">
