afb15766cbeea615b2bae89c095adf8e8a1dc329
iChanceTEK <isynera@gmail.com>
2025-12-29 22:07:31 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "memoiq",
+  "appId": "1:245370399631:web:6eec94f87db58b5d4339fa",
+  "apiKey": "AIzaSyCqCFCivy2U6b_ypgxlHnNm3Ht-pRoyE4w",
+  "authDomain": "memoiq.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "245370399631"
+};
