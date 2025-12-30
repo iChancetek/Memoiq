073e804bc2934cc1f597c163cfb04d0654d8eb4a
iChanceTEK <isynera@gmail.com>
2025-12-30 02:41:13 +0000

    Try fixing this error: `Console FirebaseError: Expected first argument t

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,7 +1,8 @@
 
 'use client';
 
-import { AuthProvider } from '@/contexts/auth-context';
+import * as React from 'react';
+import { AuthProvider, useAuth } from '@/contexts/auth-context';
 import { TaskProvider } from '@/contexts/task-context';
 import { ContactProvider } from '@/contexts/contact-context';
 import { Toaster } from './ui/toaster';
@@ -12,29 +13,45 @@ import { MemoProvider } from './memos-page';
 import { LanguageProvider } from '@/contexts/language-context';
 import { FirebaseClientProvider } from '@/firebase/client-provider';
 
+function DataProviders({ children }: { children: React.ReactNode }) {
+    const { user } = useAuth();
+
+    // Only render data providers if a user is logged in.
+    // This prevents race conditions where providers try to access Firestore
+    // before the user is authenticated.
+    if (!user) {
+        return <>{children}</>;
+    }
+
+    return (
+        <StorageProvider>
+            <TaskProvider>
+                <ContactProvider>
+                    <CalendarProvider>
+                        <MemoProvider>
+                            <MediScribeProvider>
+                                {children}
+                            </MediScribeProvider>
+                        </MemoProvider>
+                    </CalendarProvider>
+                </ContactProvider>
+            </TaskProvider>
+        </StorageProvider>
+    );
+}
+
+
 export function Providers({ children }: { children: React.ReactNode }) {
     return (
         <FirebaseClientProvider>
             <AuthProvider>
                 <LanguageProvider>
-                    <StorageProvider>
-                        <TaskProvider>
-                            <ContactProvider>
-                                <CalendarProvider>
-                                    <MemoProvider>
-                                        <MediScribeProvider>
-                                            {children}
-                                            <Toaster />
-                                        </MediScribeProvider>
-                                    </MemoProvider>
-                                </CalendarProvider>
-                            </ContactProvider>
-                        </TaskProvider>
-                    </StorageProvider>
+                    <DataProviders>
+                        {children}
+                    </DataProviders>
+                    <Toaster />
                 </LanguageProvider>
             </AuthProvider>
         </FirebaseClientProvider>
     );
 }
-
-    
