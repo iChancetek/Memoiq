d4ddecf2b1456f06193a1188b2dae4f1ee537eab
iChanceTEK <isynera@gmail.com>
2025-12-30 02:44:57 +0000

    unable to connect now

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -2,7 +2,7 @@
 'use client';
 
 import * as React from 'react';
-import { AuthProvider, useAuth } from '@/contexts/auth-context';
+import { AuthProvider } from '@/contexts/auth-context';
 import { TaskProvider } from '@/contexts/task-context';
 import { ContactProvider } from '@/contexts/contact-context';
 import { Toaster } from './ui/toaster';
@@ -13,42 +13,25 @@ import { MemoProvider } from './memos-page';
 import { LanguageProvider } from '@/contexts/language-context';
 import { FirebaseClientProvider } from '@/firebase/client-provider';
 
-function DataProviders({ children }: { children: React.ReactNode }) {
-    const { user } = useAuth();
-
-    // Only render data providers if a user is logged in.
-    // This prevents race conditions where providers try to access Firestore
-    // before the user is authenticated.
-    if (!user) {
-        return <>{children}</>;
-    }
-
-    return (
-        <StorageProvider>
-            <TaskProvider>
-                <ContactProvider>
-                    <CalendarProvider>
-                        <MemoProvider>
-                            <MediScribeProvider>
-                                {children}
-                            </MediScribeProvider>
-                        </MemoProvider>
-                    </CalendarProvider>
-                </ContactProvider>
-            </TaskProvider>
-        </StorageProvider>
-    );
-}
-
 
 export function Providers({ children }: { children: React.ReactNode }) {
     return (
         <FirebaseClientProvider>
             <AuthProvider>
                 <LanguageProvider>
-                    <DataProviders>
-                        {children}
-                    </DataProviders>
+                    <StorageProvider>
+                        <TaskProvider>
+                            <ContactProvider>
+                                <CalendarProvider>
+                                    <MemoProvider>
+                                        <MediScribeProvider>
+                                            {children}
+                                        </MediScribeProvider>
+                                    </MemoProvider>
+                                </CalendarProvider>
+                            </ContactProvider>
+                        </TaskProvider>
+                    </StorageProvider>
                     <Toaster />
                 </LanguageProvider>
             </AuthProvider>
