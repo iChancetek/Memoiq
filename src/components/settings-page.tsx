3258be78114080410a3cd5c25df94d85d60407be
iChanceTEK <isynera@gmail.com>
2025-12-30 07:02:45 +0000

    I don't see the mode option\

--- src/components/settings-page.tsx
+++ src/components/settings-page.tsx
@@ -373,7 +373,7 @@ export function SettingsPage() {
                 <div className='flex items-center space-x-2'>
                     <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
                     <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-                    <Label htmlFor="theme" className='ml-2'>{t('theme')}</Label>
+                    <Label htmlFor="theme-switch" className='ml-2'>{t('theme')}</Label>
                     <p className="text-sm text-muted-foreground capitalize">({theme} {t('mode')})</p>
                 </div>
                 <Switch
@@ -389,5 +389,7 @@ export function SettingsPage() {
     </div>
   );
 }
+
+    
 
     
