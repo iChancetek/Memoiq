7cfa7314c8fd5d88952162cf5532a7fe34a0256e
iChanceTEK <isynera@gmail.com>
2025-12-30 06:17:17 +0000

    Replace 2025 with 2026

--- src/components/auth-page.tsx
+++ src/components/auth-page.tsx
@@ -74,7 +74,7 @@ export function AuthPage() {
         </div>
       </div>
       <footer className="text-center p-4 text-xs text-muted-foreground">
-        @ MemoIQ 2025. Developed by ChanceTEK LLC I iChanceTEK I Chancellor Minus
+        @ MemoIQ 2026. Developed by ChanceTEK LLC I iChanceTEK I Chancellor Minus
       </footer>
     </div>
   );
